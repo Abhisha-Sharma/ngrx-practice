98cc6876e85247bd74a55399285ce38ff28d408e
abhisha <abhishasharma004@gmail.com>
2024-08-06 17:42:59 +0545

    Created counter using store

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,10 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CounterState } from '../state/counter.state';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.scss']
 })
-export class CounterOutputComponent {
-@Input() counter: any;
+export class CounterOutputComponent implements OnInit , OnDestroy{
+  counter!: number;
+  counterSubscription!:Subscription;
+  constructor(private store:Store<{counter:CounterState}>){}
+  
+
+  ngOnInit(): void {
+   this.counterSubscription = this.store.select('counter').subscribe(data=>{
+      this.counter = data.counter;
+    })
+  }
+  ngOnDestroy(){
+    if(this.counterSubscription){
+      this.counterSubscription.unsubscribe();
+    }
+  }
+
 }
